88d3b42abe5496eabd06cd2ff7f8f4bb661dd272
aleynaaley <aleyna.erkulll@gmail.com>
2025-10-16 16:39:50 +0300

    added comment

--- mango-clone/src/main.js
+++ mango-clone/src/main.js
@@ -1,6 +1,6 @@
-import './assets/main.css'
+import './assets/main.css' //Bu proje için genel CSS (stil) dosyasını dahil ediyor. Her sayfada geçerli olacak
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue' //Vue.js uygulamasını oluşturmak için gerekli fonksiyonu Vue kütüphanesinden alıyor
+import App from './App.vue' //Ana uygulama bileşenini (App.vue) dahil ediyor
 
-createApp(App).mount('#app')
+createApp(App).mount('#app')    //Vue uygulamasını oluşturuyor ve App bileşenini '#app' id'sine sahip HTML elementine bağlıyor
